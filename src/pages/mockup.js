98aefbcd43a3b0ff7d8edc4b37c76ef78bbcf149
lvm3632 <lvm3632@gmail.com>
2022-09-26 20:56:43 -0500

    pages: documentation, mockup, team and index

---
+++ src/pages/mockup.js
@@ -0,0 +1,37 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const pageStyles = {
+  color: "#232129",
+  padding: "96px",
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+}
+
+const paragraphStyles = {
+  marginBottom: 48,
+}
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+}
+
+const MockUp = () => {
+  return (
+    <main>
+        <h1>MockUps</h1>
+
+    </main>
+  )
+}
+
+export default MockUp
+
+export const Head = () => <title>Not found</title>
